52aee87d1410fbb8ff16c17fd1808ac698b84e41
Poonam Rajput <poonam.rajput@northcoders.com>
2024-09-26 09:17:36 +0100

    function calls to 'pet' and server calls to 'cat'

--- test/test_async.py
+++ test/test_async.py
@@ -1,6 +1,7 @@
 from time import time
-from src.async_cat_server import fetch_banner_content
+from async_pet_server import fetch_banner_content
 import pytest
+
 
 @pytest.mark.asyncio
 async def test_fetch_banner_content_returns_value_from_server():
@@ -11,11 +12,14 @@ async def test_fetch_banner_content_returns_value_from_server():
         "copyrightYear": 2006,
     }
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
 async def test_get_lower_owners_returns_list_of_owners_in_lower_case():
     lower_owners = await get_lower_owners()
-    assert lower_owners == ["pavlov", "schrodinger", "foucault", "vel", "calvin"]
+    assert lower_owners == [
+        "pavlov", "schrodinger", "foucault", "vel", "calvin"]
+
 
 @pytest.mark.skip
 @pytest.mark.asyncio
@@ -26,69 +30,77 @@ async def test_get_pets_by_owner_returns_dict_with_requested_owners_pets():
     pets = await get_pets_by_owner("schrodinger")
     assert pets == {"pets": ["Leben", "Tot"]}
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
 async def test_get_pets_by_owner_returns_dict_with_empty_list_when_no_owner():
     res = await get_pets_by_owner("missing")
     assert res == {"pets": [], "message": "missing not found"}
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
-async def test_get_all_cats_returns_dict_with_correct_ordered_owners_and_cats():
-    res = await get_all_cats()
+async def test_get_all_pets_returns_dict_with_correct_ordered_owners_and_cats():
+    res = await get_all_pets()
     assert res == [
-        {"owner": "pavlov", "cats": ["Belle", "Dribbles", "Nibbles"]},
-        {"owner": "schrodinger", "cats": ["Leben", "Tot"]},
-        {"owner": "foucault", "cats": ["M. Fang"]},
-        {"owner": "vel", "cats": ["Opal"]},
-        {"owner": "calvin", "cats": ["Hobbes"]},
+        {"owner": "pavlov", "pets": ["Belle", "Dribbles", "Nibbles"]},
+        {"owner": "schrodinger", "pets": ["Leben", "Tot"]},
+        {"owner": "foucault", "pets": ["M. Fang"]},
+        {"owner": "vel", "pets": ["Opal"]},
+        {"owner": "calvin", "pets": ["Hobbes"]},
     ]
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
-async def test_get_all_cats_runs_multiple_requests_for_pets_at_the_same_time():
+async def test_get_all_pets_runs_multiple_requests_for_pets_at_the_same_time():
     start_time = time()
-    await get_all_cats()
+    await get_all_pets()
     execution_time = time() - start_time
     allowed_time = 7
-    # up to 3 for owners and up to 3 for cats and 1 for leeway
+    # up to 3 for owners and up to 3 for pets and 1 for leeway
     assert execution_time < allowed_time
 
-@pytest.mark.skip
-@pytest.mark.asyncio
-async def test_get_cat_pics_returns_cat_pics_from_servedr():
-    cat_pics = await get_cat_pics(["cute_cat"])
-    assert cat_pics == ["cute_cat.png"]
 
 @pytest.mark.skip
 @pytest.mark.asyncio
-async def test_get_cat_pics_returns_placeholder():
-    cat_pics = await get_cat_pics(["sausage_dog"])
-    assert cat_pics == [
+async def test_get_pet_pics_returns_cat_pics_from_server():
+    pet_pics = await get_pet_pics(["cute_cat"])
+    assert pet_pics == ["cute_cat.png"]
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_get_pet_pics_returns_placeholder():
+    pet_pics = await get_pet_pics(["sausage_dog"])
+    assert pet_pics == [
         "placeholder_cat.png",
     ]
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
-async def test_get_cat_pics_handles_placeholders_and_regular_images():
-    cat_pics = await get_cat_pics(
+async def test_get_pet_pics_handles_placeholders_and_regular_images():
+    pet_pics = await get_pet_pics(
         ["cute_cat", "grumpy_cat", "smelly_cat", "sausage_dog"]
     )
-    assert cat_pics == [
+    assert pet_pics == [
         "cute_cat.png",
         "grumpy_cat.png",
         "smelly_cat.png",
         "placeholder_cat.png",
     ]
 
+
 @pytest.mark.skip
 @pytest.mark.asyncio
-async def test_get_cat_pics_runs_multiple_requests_for_pics_at_the_same_time():
+async def test_get_pet_pics_runs_multiple_requests_for_pics_at_the_same_time():
     start_time = time()
-    res = await get_cat_pics(["cute_cat", "grumpy_cat", "smelly_cat", "sausage_dog"])
+    res = await get_pet_pics(["cute_cat", "grumpy_cat", "smelly_cat", "sausage_dog"])
     execution_time = time() - start_time
     # up to 3 seconds to request pics and 1 for leeway
     assert execution_time < 4
+
 
 @pytest.mark.skip
 @pytest.mark.asyncio
